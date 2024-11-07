93ae8bf1bd92d131550c345ed15b4a0169f9f062
Tanapon Rattanamano <poomtanapon2.7.44@gmail.com>
2024-11-07 13:54:33 +0700

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3000   
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors(corsOptions))
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+  };
+
+
+  // simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+  });
+  
+  // set port, listen for requests
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server is running http://localhost:${PORT}`);
+  });
+
